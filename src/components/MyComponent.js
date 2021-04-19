a8536c362a5e3c18891c138b4454723ac4fb6958
Jerusalem Darkera <jdarkera@gmail.com>
2021-04-19 12:50:13 -0700

    Commit #2

--- src/components/MyComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default class MyComponent extends React.Component {
-render() {
- return <div><input
- onChange={(event) => {this.setState({input: event.target.value})}}
- type="text"/></div>;
- }
-}
